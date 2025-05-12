547f1c4d7b57cb7358c9795cc208803f315fad38
Ruslan <ruslan221079@gmail.com>
2025-05-12 10:19:23 +0300

    12345

--- routes/web.php
+++ routes/web.php
@@ -1,103 +1,78 @@
 <?php
 
-namespace App\Http\Controllers;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\SubscriberController;
+use App\Http\Controllers\TelegramController;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use App\Services\TelegramService;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-class TelegramController extends Controller
-{
-    private $telegramService;
-    private $botId; // Хранение ID бота
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-    public function __construct(TelegramService $telegramService)
-    {
-        // Внедрим TelegramService через конструктор
-        $this->telegramService = $telegramService;
+/* Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); */
 
-        // Получаем ID бота при инициализации контроллера
-        $this->botId = $this->telegramService->getBotId();
-        Log::info("Идентификатор бота: {$this->botId}");
+
+Route::middleware(['auth'])->group(
+    function () {
+        Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+
+        Route::post('/filtering_user', [UserController::class, 'filtering_user'])->name('filtering_user');
     }
+);
 
-    // Обработка входящих обновлений от Telegram
-    public function handleWebhook(Request $request)
-    {
-        // Получаем все данные обновления от Telegram
-        $update = $request->all();
 
-        // Логируем обновления для отладки
-        Log::info('Получено обновление Telegram: ', $update);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-        // Проверяем наличие массива `channel_post` или `message`
-        if (isset($update['channel_post'])) {
-            $chatId = $update['channel_post']['chat']['id'];
-            $this->sendMessage($chatId, "Добро пожаловать в канал, {$chatId}!");
-        }
+/* my routes */
+Route::get('/test', [TestController::class, 'test']);
 
-        // Проверяем наличие новых участников чата
-        if (isset($update['message']['new_chat_members'])) {
-            foreach ($update['message']['new_chat_members'] as $newMember) {
-                $chatId = $update['message']['chat']['id'];
-                $userId = $newMember['id'];
-                $firstName = $newMember['first_name'] ?? 'Пользователь';
+Route::get('/test-token', function () {
+    return env('TELEGRAM_BOT_TOKEN', 'Токен не задан');
+});
 
-                // Игнорируем события, если это бот
-                if ($userId == $this->botId) {
-                    Log::info("Игнорируем событие, так как это бот: {$userId}");
-                    continue;
-                }
 
-                Log::info("Новый подписчик с ID: {$userId}");
+/** Роут для отправки писем с главной */
+Route::post('/submit-form', [RequestController::class, 'submitForm'])->name('submit.form');
 
-                // Ваши действия (например, отправка в Яндекс Метрику)
-                $this->sendToYandexMetrica($userId, $firstName);
-                $this->sendMessage($chatId, "Добро пожаловать, {$firstName}!");
-            }
-        }
 
-        return response()->json(['status' => 'ok']); // Возвращаем успешный ответ
-    }
+Route::get('/subscribers', [SubscriberController::class, 'index']);
 
-    // Метод для отправки сообщений через TelegramService
-    private function sendMessage($chatId, $text)
-    {
-        try {
-            // Используем TelegramService для отправки сообщения
-            $this->telegramService->sendMessage($chatId, $text);
-            Log::info("Сообщение отправлено успешно: ChatID {$chatId}");
-        } catch (\Exception $e) {
-            Log::error("Ошибка отправки сообщения: {$e->getMessage()}");
-        }
-    }
+Route::get('/clear', function () {
 
-    // Метод для отправки данных в Яндекс Метрику
-    private function sendToYandexMetrica($userId, $userName)
-    {
-        // Укажите ваш ID счетчика и цель
-        $counterId = '101725200';  // Замените на ваш ID счетчика
-        $goalId = 'new_subscriber';  // Замените на вашу цель
+    Artisan::call('cache:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    return "Сброс кэша выполнен!";
+});
 
-        // URL для отправки данных в Яндекс Метрику
-        $url = "https://mc.yandex.ru/watch/{$counterId}";
 
-        // Параметры для отправки
-        $data = [
-            't' => 'goal',                 // Тип отправляемых данных
-            'id' => $goalId,               // ID цели
-            'uid' => $userId,              // ID пользователя
-            'url' => 'https://t.me/+GBRMKva5zohjNjMy', // URL вашего канала
-            'g' => 'new_subscriber',        // Уникальный идентификатор события
-            'name' => $userName,           // Имя пользователя
-        ];
-
-        try {
-            // Отправка данных в Яндекс Метрику
-            $response = file_get_contents($url . '?' . http_build_query($data));
-            Log::info('Отправка в Yandex Metrica: ', ['response' => $response]);
-        } catch (\Exception $e) {
-            Log::error("Ошибка отправки в Яндекс Метрику: {$e->getMessage()}");
-        }
-    }
-}
+require __DIR__ . '/auth.php';
